14385d4d2e97632b2111f978354c14de1cab3262
Dinar, Permadi <sdnteja2@gmail.com>
2023-10-20 20:40:29 +0700

    main

--- storyblok/GaleriList.vue
+++ storyblok/GaleriList.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="text-center">
-    <img
+    <NuxtImg
       @click="isOpen = true"
       v-if="blok.foto?.filename"
-      class="rounded-md sm:h-48 lg:h-60 mx-auto object-cover bg-cover bg-center"
+      class="rounded-md sm:h-48 w-64 mx-auto object-cover bg-cover object-center bg-center"
       :src="blok.foto.filename + '/m/500x0/smart'"
       :alt="blok.nama"
       height="450"
-      width="230"
+      width="450"
       loading="lazy" />
     <div class="mt-2 sm:mt-4">
-      <h2 class="text-sm font-medium sm:text-base lg:text-lg">
-        {{ blok.nama }}
-      </h2>
-      <p class="text-xs sm:text-sm lg:text-base">
-        {{ blok.jabatan }}
-      </p>
+      <h3 class="text-sm font-medium sm:text-base lg:text-lg">
+        {{ blok.title }}
+      </h3>
     </div>
   </div>
   <UModal
@@ -39,16 +36,7 @@
           class="rounded-md object-cover bg-center" />
         <div class="text-center">
           <p class="text-base font-semibold leading-6">
-            {{ blok.nama }}
-          </p>
-          <p class="text-base font-semibold leading-6">
-            {{ blok.nip }}
-          </p>
-          <p class="text-base font-semibold leading-6">
-            {{ blok.jabatan }}
-          </p>
-          <p class="text-base font-semibold leading-6">
-            {{ blok.pendidikan }}
+            {{ blok.title }}
           </p>
         </div>
       </div>
---
+++ storyblok/GaleriPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <div v-editable="blok" :class="svgClass">
+      <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
+        <div class="mt-5 max-w-2xl text-center mx-auto">
+          <h1
+            v-editable="blok"
+            class="block headline text-4xl md:text-5xl lg:text-6xl">
+            {{ blok.headline }}
+          </h1>
+        </div>
+      </div>
+    </div>
+    <div class="max-w-[85rem] px-2 py-10 md:px-14 lg:py-14 mx-auto">
+      <div class="">
+        <div v-editable="blok" class="grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-12">
+          <StoryblokComponent
+            v-for="blok in blok.list"
+            :key="blok._uid"
+            :blok="blok" />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+<script setup>
+  const props = defineProps({ blok: Object });
+  const { svgClass } = useSvg(ref(props.blok));
+</script>
